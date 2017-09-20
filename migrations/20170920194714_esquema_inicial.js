141ca5a89cbd7e819958ea6d1232d7c93a278b86
alcivanio <alcivaniosilva@gmail.com>
2017-09-20 20:10:23 -0300

    Commit inicial

---
+++ migrations/20170920194714_esquema_inicial.js
@@ -0,0 +1,8 @@
+exports.up = knex => knex.schema.createTable("contato", tb => {
+  tb.increments("idcontato")
+  tb.string("nomecontato")
+  tb.string("enderecocontato")
+  tb.string("telefonecontato")
+})
+
+exports.down = knex => knex.schema.dropTable("contato")
